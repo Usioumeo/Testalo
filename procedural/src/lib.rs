ea6a17f9df804d7837f0200c29c6965cf06d04fb
Alessio Zeni <alessio.zeni01@gmail.com>
2024-10-11 13:09:11 +0200

    procedural macros

--- procedural/src/lib.rs
+++ procedural/src/lib.rs
@@ -1,9 +1,7 @@
-use core::prelude;
-
 use proc_macro::TokenStream;
-use syn::{fold::{fold_file, fold_item_mod, Fold}, parse, parse_quote, parse_str, File, Ident, ItemMod};
+use syn::{fold::Fold, parse, parse_quote, parse_str, File, Ident, ItemMod};
 use rust_default::prelude::*;
-use quote::{format_ident, quote, ToTokens};
+use quote::{format_ident, ToTokens};
 
 struct Sanitizer{
 
@@ -16,30 +14,6 @@ impl Fold for Sanitizer{
         }
         i
     }
-    /*fn fold_file(&mut self, mut i: syn::File) -> syn::File {
-        i.items.retain(|x| 
-            if let syn::Item::ExternCrate(e) = x{
-                let ident: Ident = parse_quote!(std);
-                e.ident!=ident
-        }else{
-            true
-        });
-        fold_file(self, i)
-    }
-    fn fold_item_mod(&mut self, mut i: syn::ItemMod) -> syn::ItemMod {
-        if let Some((_, items)) = &mut i.content{
-            items.retain(|x| {
-                //panic!("folding {}", x.to_token_stream());
-                if let syn::Item::ExternCrate(e) = x{
-                    
-                    let ident: Ident = parse_quote!(std);
-                    e.ident!=ident
-            }else{
-                true
-            }});
-        }
-        fold_item_mod(self, i)
-    }*/
 }
 
 #[proc_macro_attribute]
@@ -51,11 +25,7 @@ pub fn magic_macro(_attr: TokenStream, item: TokenStream) -> TokenStream {
         attrs: Vec::new(),
         items: input,
     };
-    //let input = item.to_string();
-    //panic!("{}", input);
-    //println!("{}", input);
-    //let mut prelude = parse_str::<File>(&prelude).unwrap().to_token_stream();
-    //panic!("{}", item);
+
     let input = file.to_token_stream().to_string();
     let ex = RustExercise2::parse(&input).unwrap();
     let test = GeneratedFiles2::generate(ex, input).unwrap();
@@ -68,28 +38,16 @@ pub fn magic_macro(_attr: TokenStream, item: TokenStream) -> TokenStream {
     for (name, (content, _)) in test.files{
         let name = format_ident!("{}", name);
         let content: File = parse_str(&content).unwrap();
-        //println!("{} {}",name,  content);
+
         let module: ItemMod = parse_quote! {mod #name {
             #content
         }};
-        //println!("{}", quote.to_token_stream());
-        /*let module: ItemMod = parse2().map_err(|x| {
-            let l = x.span().start();
-            x.to_compile_error().to_string()+&format!("{} {}", l.column, l.line)
-        }).unwrap();*/
         file.items.push(module.into());
         
     }
-    /*let prelude = parse_quote! {
-        extern crate std;
 
-    };*/
     let mut s = Sanitizer{};
     let file = s.fold_file(file);
-    //panic!("{}", prettyplease::unparse(&file));
-    //panic!("{:?}", test);
-    //item
-    //prelude.extend();
-    //prelude.into()
+
     file.into_token_stream().into()
 }
